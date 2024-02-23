c1e53a711f9c01c66081fe15517fcdb38450439c
whyiamavirgin <mrxcl1986@gmail.com>
2024-02-23 12:55:36 +0300

    changed hour forecast

--- src/Weather.js
+++ src/Weather.js
@@ -345,6 +345,34 @@ const Weather = () => {
         
       )} 
 
+        {weatherForecast ? (
+          <div 
+          className='dayForecast' style={{
+            background: hourForecastBgColor,
+            color: "#fff"
+          }}>
+            <p className='hourText'>
+            <svg width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M8.5 4.33333V8.5H12.6667M8.5 16C4.35786 16 1 12.6421 1 8.5C1 4.35786 4.35786 1 8.5 1C12.6421 1 16 4.35786 16 8.5C16 12.6421 12.6421 16 8.5 16Z" stroke="#CCCCCC" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+            ПОЧАСОВОЙ ПРОГНОЗ
+            </p>
+            <div className='updateBlock'>
+                <p className='update'>
+                  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                  <path d="M16 20V18M18 16H20M7.04996 11.293L5.63574 12.7072C4.07365 14.2693 4.07466 16.8016 5.63675 18.3637C7.19885 19.9258 9.7308 19.9262 11.2929 18.3641L12.7076 16.9497M6 8H4M8 4V6M11.293 7.05044L12.7072 5.63623C14.2693 4.07413 16.8016 4.07368 18.3637 5.63578C19.9258 7.19787 19.9254 9.7308 18.3633 11.2929L16.9492 12.7071" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                  </svg>
+                В Разработке
+                </p>
+            </div>
+          </div>
+        ) : (
+          <div className='dayForecast' style={{
+            background: "#282A65",
+            color: "#FFF"
+          }}></div>
+        )}
+
        {/* {weatherForecast ? (
         <div className='dayForecast' style={{
           background: hourForecastBgColor,

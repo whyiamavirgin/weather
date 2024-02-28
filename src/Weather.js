dd6c96233faff80dc24aa758f818d8b3ec18c7df
whyiamavirgin <mrxcl1986@gmail.com>
2024-02-28 11:40:20 +0300

    added some blocks

--- src/Weather.js
+++ src/Weather.js
@@ -9,6 +9,8 @@ import "swiper/css/parallax"
 import { Swiper, SwiperSlide} from 'swiper/react';
 import moment from 'moment'
 import 'moment/locale/ru'
+import Box from '@mui/material/Box';
+import Slider from '@mui/material/Slider';
 
 // Импортируем файлы для облачной погоды
 
@@ -50,7 +52,7 @@ moment.locale('ru')
 
 const Weather = () => {
 
-  const version = '0.3.3'
+  const version = '0.4.0'
 
   const imageAttribution = "Image by wirestock"
 
@@ -75,6 +77,9 @@ const Weather = () => {
   let localDayVar
   let localDayVarInt
   let localMonthVar
+  let uvIndex 
+  let uvIndexLowHigh
+  let uvColor
 
   let weekDay = moment().format('dddd').toUpperCase()
 
@@ -276,7 +281,38 @@ const Weather = () => {
 
     localDayVar = parseInt(weatherForecast.location.localtime.slice(8,10))
     localMonthVar = parseInt(weatherForecast.location.localtime.slice(5,7))
+    uvIndex = weatherForecast.current.uv
+
+    switch(uvIndex) {
+      case 1:
+      case 2:
+        uvIndexLowHigh = "Низкий"
+        uvColor = "#70e000"
+        break
+      case 3:
+      case 4:
+      case 5:
+        uvIndexLowHigh = "Умеренный"
+        uvColor = "#fcf300"
+        break
+      case 6:
+      case 7:
+        uvIndexLowHigh = "Высокий"
+        uvColor = "#ff7b00"
+        break
+      case 8:
+      case 9:
+      case 10:
+        uvIndexLowHigh = "Очень высокий"
+        uvColor = "#bf0603"
+        break
+      default:
+        uvIndexLowHigh = "Экстремальный"
+        uvColor = "#7b2cbf"
+        break
+    }
   }  
+
 
   useEffect(() => {
     setTimeout(() => {
@@ -519,6 +555,88 @@ const Weather = () => {
 
       </div>
       )}
+
+      {weatherForecast ?  (
+        <div className='uvInfo'>
+          <div className='uvIndex'>
+            <p className='dayliText'>
+              <svg className='sunSVG' width="14" height="14" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M6 11C6 13.7614 8.23858 16 11 16C13.7614 16 16 13.7614 16 11C16 8.23858 13.7614 6 11 6C8.23858 6 6 8.23858 6 11Z" fill="white"/>
+              <path d="M11 3V1M11 19V21M5.41421 5.41421L4 4M16.728 16.728L18.1422 18.1422M3 11H1M19 11H21M16.7285 5.41421L18.1427 4M5.4147 16.728L4.00049 18.1422M11 16C8.23858 16 6 13.7614 6 11C6 8.23858 8.23858 6 11 6C13.7614 6 16 8.23858 16 11C16 13.7614 13.7614 16 11 16Z" stroke="white" stroke-linecap="round" stroke-linejoin="round"/>
+              </svg>
+
+                УФ-ИНДЕКС
+              </p>
+              <p className='uvIndexNum'>{uvIndex}</p>
+              <p className='uvIndexHigh'>{uvIndexLowHigh}</p>
+              <div className='slider'
+              
+              style={{
+                color: uvColor
+              }}>
+                <Slider
+                  size='small'
+                  aria-label="UV"
+                  defaultValue={uvIndex * 10 }
+                  disabled
+                />
+              </div>
+          </div>
+          <div className='sunRise'>
+            <p className='dayliText'>
+              <svg className='sunSVG' width="18" height="18" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path d="M36.75 33.25H5.25C4.78587 33.25 4.34075 33.0656 4.01257 32.7374C3.68438 32.4092 3.5 31.9641 3.5 31.5C3.5 31.0358 3.68438 30.5907 4.01257 30.2625C4.34075 29.9343 4.78587 29.75 5.25 29.75H36.75C37.2141 29.75 37.6592 29.9343 37.9874 30.2625C38.3156 30.5907 38.5 31.0358 38.5 31.5C38.5 31.9641 38.3156 32.4092 37.9874 32.7374C37.6592 33.0656 37.2141 33.25 36.75 33.25Z" fill="white"/>
+              <path d="M36.75 26.25H35C34.5359 26.25 34.0907 26.0656 33.7625 25.7374C33.4344 25.4093 33.25 24.9641 33.25 24.5C33.25 24.0359 33.4344 23.5908 33.7625 23.2626C34.0907 22.9344 34.5359 22.75 35 22.75H36.75C37.2141 22.75 37.6592 22.9344 37.9874 23.2626C38.3156 23.5908 38.5 24.0359 38.5 24.5C38.5 24.9641 38.3156 25.4093 37.9874 25.7374C37.6592 26.0656 37.2141 26.25 36.75 26.25ZM7 26.25H5.25C4.78587 26.25 4.34075 26.0656 4.01257 25.7374C3.68438 25.4093 3.5 24.9641 3.5 24.5C3.5 24.0359 3.68438 23.5908 4.01257 23.2626C4.34075 22.9344 4.78587 22.75 5.25 22.75H7C7.46413 22.75 7.90925 22.9344 8.23744 23.2626C8.56563 23.5908 8.75 24.0359 8.75 24.5C8.75 24.9641 8.56563 25.4093 8.23744 25.7374C7.90925 26.0656 7.46413 26.25 7 26.25ZM30.905 16.345C30.4703 16.3253 30.0585 16.1444 29.75 15.8375C29.424 15.5096 29.2411 15.0661 29.2411 14.6038C29.2411 14.1414 29.424 13.6979 29.75 13.37L30.9925 12.1275C31.1494 11.9443 31.3424 11.7955 31.5595 11.6905C31.7766 11.5854 32.0131 11.5264 32.2541 11.5171C32.4951 11.5078 32.7355 11.5484 32.9601 11.6364C33.1846 11.7244 33.3886 11.8578 33.5591 12.0284C33.7297 12.1989 33.8631 12.4029 33.9511 12.6274C34.0391 12.852 34.0797 13.0923 34.0704 13.3333C34.0611 13.5744 34.002 13.8108 33.897 14.028C33.792 14.2451 33.6432 14.4381 33.46 14.595L32.2175 15.8375C32.0457 16.0083 31.8404 16.1416 31.6145 16.229C31.3886 16.3163 31.147 16.3559 30.905 16.345ZM11.095 16.345C10.6359 16.3431 10.196 16.1608 9.87 15.8375L8.6275 14.595C8.3408 14.2602 8.19099 13.8296 8.20801 13.3892C8.22502 12.9487 8.4076 12.5309 8.71926 12.2193C9.03093 11.9076 9.44872 11.725 9.88916 11.708C10.3296 11.691 10.7602 11.8408 11.095 12.1275L12.3375 13.37C12.6634 13.6979 12.8464 14.1414 12.8464 14.6038C12.8464 15.0661 12.6634 15.5096 12.3375 15.8375C12.174 15.9997 11.9801 16.128 11.7668 16.2151C11.5536 16.3022 11.3253 16.3463 11.095 16.345ZM21 12.25C20.5359 12.25 20.0907 12.0656 19.7626 11.7374C19.4344 11.4093 19.25 10.9641 19.25 10.5V8.75C19.25 8.28587 19.4344 7.84075 19.7626 7.51256C20.0907 7.18437 20.5359 7 21 7C21.4641 7 21.9092 7.18437 22.2374 7.51256C22.5656 7.84075 22.75 8.28587 22.75 8.75V10.5C22.75 10.9641 22.5656 11.4093 22.2374 11.7374C21.9092 12.0656 21.4641 12.25 21 12.25Z" fill="white"/>
+              <path d="M28 23.3275C28.0104 25.0471 27.3874 26.7103 26.25 28H15.75C14.6125 26.7103 13.9896 25.0471 14 23.3275C14 21.471 14.7375 19.6905 16.0502 18.3777C17.363 17.065 19.1435 16.3275 21 16.3275C22.8565 16.3275 24.637 17.065 25.9497 18.3777C27.2625 19.6905 28 21.471 28 23.3275Z" fill="white"/>
+              </svg>
+
+              {weatherForecast.current.is_day ? ( "ЗАКАТ") : ("ВОСХОД")}
+            </p>
+            <p className='uvIndexNum'>
+              {weatherForecast.current.is_day ? (weatherForecast.forecast.forecastday[0].astro.sunset) : (weatherForecast.forecast.forecastday[0].astro.sunrise)}
+            </p>
+            <p className='uvIndexHigh'>
+            {weatherForecast.current.is_day ? ("Восход " + weatherForecast.forecast.forecastday[1].astro.sunrise) : ("Закат " + weatherForecast.forecast.forecastday[1].astro.sunset)}  
+            </p>
+          </div>
+        </div>
+      ) : (
+        <div></div>
+      )}
+
+      <div className='wind'>
+        {weatherForecast ? (
+          <div className='windBlock'>
+            <p className='dayliText'>
+              <svg className='sunSVG' width="18" height="16" viewBox="0 0 23 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+              <path fill-rule="evenodd" clip-rule="evenodd" d="M5.34884 3.47674C5.34884 1.5566 6.90542 0 8.82558 0C10.7457 0 12.3023 1.55659 12.3023 3.47674C12.3023 5.3969 10.7457 6.95349 8.82558 6.95349H1.87209C1.42898 6.95349 1.06977 6.59427 1.06977 6.15116C1.06977 5.70805 1.42898 5.34884 1.87209 5.34884H8.82558C9.85951 5.34884 10.6977 4.51067 10.6977 3.47674C10.6977 2.44281 9.85951 1.60465 8.82558 1.60465C7.79165 1.60465 6.95349 2.44281 6.95349 3.47674V3.8588C6.95349 4.30192 6.59427 4.66113 6.15116 4.66113C5.70805 4.66113 5.34884 4.30192 5.34884 3.8588V3.47674ZM13.907 5.61628C13.907 3.10531 15.9425 1.06977 18.4535 1.06977C20.9644 1.06977 23 3.10531 23 5.61628C23 8.12725 20.9644 10.1628 18.4535 10.1628H0.802326C0.359217 10.1628 0 9.80356 0 9.36046C0 8.91737 0.359217 8.55814 0.802326 8.55814H18.4535C20.0783 8.55814 21.3953 7.24102 21.3953 5.61628C21.3953 3.99154 20.0783 2.67442 18.4535 2.67442C16.8287 2.67442 15.5116 3.99154 15.5116 5.61628V6.15116C15.5116 6.59427 15.1524 6.95349 14.7093 6.95349C14.2662 6.95349 13.907 6.59427 13.907 6.15116V5.61628ZM2.13953 12.5698C2.13953 12.1267 2.49875 11.7674 2.94186 11.7674H18.4535C20.9644 11.7674 23 13.803 23 16.314C23 18.8249 20.9644 20.8605 18.4535 20.8605C15.9425 20.8605 13.907 18.8249 13.907 16.314V15.7791C13.907 15.336 14.2662 14.9767 14.7093 14.9767C15.1524 14.9767 15.5116 15.336 15.5116 15.7791V16.314C15.5116 17.9387 16.8287 19.2558 18.4535 19.2558C20.0783 19.2558 21.3953 17.9387 21.3953 16.314C21.3953 14.6892 20.0783 13.3721 18.4535 13.3721H2.94186C2.49875 13.3721 2.13953 13.0129 2.13953 12.5698Z" fill="white"/>
+              </svg>
+              ВЕТЕР
+            </p>
+            <div className='wind_speed'>
+              <div className='wind_speed_num'>
+                  <p>{weatherForecast.current.wind_kph}</p>
+              </div>
+              <div className='wind_speed_text'>
+                <p>км/ч</p>
+                <p>Ветер</p>
+              </div>
+            </div>
+            <div className='white_line_second'></div>
+            <div className='wind_speed'>
+              <div className='wind_speed_num'>
+                  <p>{weatherForecast.current.gust_kph}</p>
+              </div>
+              <div className='wind_speed_text'>
+                <p>км/ч</p>
+                <p>Порывы ветра</p>
+              </div>
+            </div>
+          </div>
+        ) : (
+          <div></div>
+        )}
+      </div>
 
     <div className='info'>
             <div className='powered'>
